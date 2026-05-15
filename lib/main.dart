6e060fe00dacb433a96c66108bca7a89716adda7
IsaacRJ <159511532+whitebox8@users.noreply.github.com>
2026-05-15 01:34:05 -0600

    3.7
    Integración inicial de Firebase en Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MiApp());
 }
 
